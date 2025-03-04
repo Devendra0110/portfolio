6df841fc23bc9882afaba7de2e5dd56907d34be1
Devendra Gaud <devendragaud@externlabs.com>
2025-03-04 15:45:08 +0530

    image issue fixed

---
+++ src/images/images.jsx
@@ -0,0 +1,14 @@
+const IMAGES = {
+    iwbms: new URL('./iwbms.png', import.meta.url).href,
+    eo4u: new URL('./eo4u.png', import.meta.url).href,
+    primus: new URL('./primus.png', import.meta.url).href,
+    entercraft: new URL('./entercraft.png', import.meta.url).href,
+    spaceup: new URL('./spaceup.png', import.meta.url).href,
+    sheideo: new URL('./sheideo.png', import.meta.url).href,
+    magnet: new URL('./magnet.png', import.meta.url).href,
+    goodvibes: new URL('./goodvibes.png', import.meta.url).href,
+    fms: new URL('./fms.png', import.meta.url).href,
+    leak: new URL('./leak.png', import.meta.url).href,
+    nuegocms: new URL('./nuegocms.png', import.meta.url).href,
+}
+export default IMAGES
